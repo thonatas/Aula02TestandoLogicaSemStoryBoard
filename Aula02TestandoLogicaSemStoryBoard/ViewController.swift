02238b48a53fb5cc04988325aaab417f13771343
thonatas <thonatas_borges@outlook.com>
2021-06-10 21:22:19 -0300

    Initial Commit

---
+++ Aula02TestandoLogicaSemStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula02TestandoLogicaSemStoryBoard
+//
+//  Created by Thonatas Borges on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
